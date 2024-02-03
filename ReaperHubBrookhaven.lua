db17375d432033d281ba5e31e916d7f3e69ff005
189hi <76269179+189hi771@users.noreply.github.com>
2024-02-03 04:30:57 +0200

    Update ReaperHubBrookhaven.lua

--- ReaperHubBrookhaven.lua
+++ ReaperHubBrookhaven.lua
@@ -1,5 +1,6 @@
 -- Variables
 local starterGui = game:GetService("StarterGui")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 starterGui:SetCore("SendNotification", {
     Title = "Reaper Hub",
@@ -8,51 +9,10 @@ starterGui:SetCore("SendNotification", {
     Duration = 7
 })
 
-local uilibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiet1308/tvkhub/main/rac"))()
-local windowz = uilibrary:CreateWindow("UI Library", "Game Name", true)
+local Window = OrionLib:MakeWindow({Name = "Reaper Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "ReaperHub"})
 
-local Page1 = windowz:CreatePage("Page I")
-
-
-local Section1 = Page1:CreateSection("Section Name")
-
-Section1:CreateSlider("Slider Example", {Min = 16, Max = 500, DefaultValue = 30}, function(Value)
-   print(Value)
-end)
-
-Section1:CreateToggle("Toggle Example", {Toggled=false , Description = false}, function(Value)
-   print(Value)
-end)
-
-Section1:CreateButton("Button Example", function ()
-   print("Button Cliked!")
-end)
-
-Section1:CreateTextbox("TextBox", false, function (vv)
-   print(vv)
-end)
-
-Section1:CreateDropdown("Dropdown ", {
-   List = {"Value1", "Value2", "Value3", "Value4"},
-   Default = "None"}, function(value)
-       print(value)
-end)
-
-Section1:CreateColorPicker("Color Picker", Color3.fromRGB(255, 255, 255), function ()
-   print("fsf")
-end)
-
-local dropdown = Section1:CreateDropdown("Refresh Dropdown ", {
-   List = {"Value1", "Value2", "Value3", "Value4"},
-   Default = "None"}, function(value)
-       print(value)
-end)
-
-
-
-Section1:CreateButton("Refresh Example", function ()
-   local newlist = {"resf", "uwua", "fsk"}
-   dropdown:Clear()
-   wait(1)
-   dropdown:Add(newlist)
-end)
+local Tab = Window:MakeTab({
+	Name = "Test",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})

b2d5afbfe005f3f3256159357cde0d6683f319e7
189hi <76269179+189hi771@users.noreply.github.com>
2024-02-03 04:11:04 +0200

    Update ReaperHubBrookhaven.lua

--- ReaperHubBrookhaven.lua
+++ ReaperHubBrookhaven.lua
@@ -4,6 +4,6 @@ local starterGui = game:GetService("StarterGui")
 starterGui:SetCore("SendNotification", {
     Title = "Reaper Hub",
     Text = "Reaper Hub has loaded",
-    Icon = "rbxassetid://16218109753",
+    Icon = "rbxassetid://16218109692",
     Duration = 7
 })
